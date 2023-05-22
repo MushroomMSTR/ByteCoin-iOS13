eaed556173b2e82bb10aca5ebe4ecec6c22ba44d
NazarStf <hworker98@gmail.com>
2023-05-22 14:16:28 +0300

    done

---
+++ ByteCoin/Model/CoinData.swift
@@ -0,0 +1,13 @@
+//
+//  CoinData.swift
+//  ByteCoin
+//
+//  Created by NazarStf on 15.05.2023.
+//  Copyright © 2023 The App Brewery. All rights reserved.
+//
+
+import Foundation
+
+struct CoinData: Decodable {
+	let rate: Double
+}
